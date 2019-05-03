9003d2f021d65b439ea76807aecec6e04398b539
dustyyeti <yeti.shyn@gmail.com>
2019-05-02 23:52:58 -0700

    .

--- public/examples/beachday.js
+++ public/examples/beachday.js
@@ -62,8 +62,8 @@ export let beachday = {
 			if(!window.webkit && args.blox.mesh) args.blox.mesh.visible = true
 		},
 		sky:{
-			art:"./art/ENV008_Refl.jpg",
-			visible:false
+			art:"./art/eso0932a.jpg",
+			// visible:false
 		},
 	},
 
